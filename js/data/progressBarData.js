4db96b0797e2d8e0258c3b9047b82fc0d3efa3f0
Eduardas <latoanas@gmail.com>
2021-02-10 23:58:20 +0200

    ProgressBarData (beginning)

---
+++ js/data/progressBarData.js
@@ -0,0 +1,16 @@
+const progressBarData = [
+   {
+      label: 'LUX Design',
+      value: 90
+   },
+   {
+      label: 'Web design',
+      value: 86
+   }
+   {
+      label: 'Web developent',
+      value: 50
+   },
+];
+
+export { progressBarData }
