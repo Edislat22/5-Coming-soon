eff9882ae8c4dda6858154fb72332598f0fb2127
Eduardas <edislat22@gmail.com>
2021-02-11 23:53:59 +0200

    ProgressBar update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 import { clock } from './components/clock.js';
+import { ProgressBar } from './components/ProgressBar.js';
+import { progressBarData } from './data/progressBarData.js';
 import { socialsData } from './data/socialsData.js';
 import { renderSocials } from './components/renderSocials.js';
 
 clock('.clock', '01-04 14:00:00');
+const progress = new ProgressBar('.left-column', progressBarData);
 renderSocials('footer > .socials', socialsData);
