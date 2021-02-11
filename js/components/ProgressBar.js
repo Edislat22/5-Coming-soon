779e3013ee285753385f17282b8b1d1d6682e26f
Eduardas <edislat22@gmail.com>
2021-02-12 00:20:28 +0200

    ProgressBar update

--- js/components/ProgressBar.js
+++ js/components/ProgressBar.js
@@ -1,6 +1,17 @@
 class ProgressBar {
-    constructor() {
+    constructor(selector, data) {
+       this.selector = selector;
+       this.data = data;
+    }
 
+
+    isValidSelector() {
+        if (typeof this.selector !== 'string' ||
+        this.selector === '') {
+        console.error('ERROR: netinkamo formato selectorius.');
+        return false;        }
+    }
+         return true;
     }
 }
 
