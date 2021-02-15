64255858d524e0ee83bc0bd70317510356356b02
Eduardas <edislat22@gmail.com>
2021-02-16 01:00:46 +0200

    Progress bar validation #56

--- js/data/progressBarData.js
+++ js/data/progressBarData.js
@@ -1,6 +1,65 @@
 const progressBarData = [
+   141526,
+   'asdf',
+   true,
+   false,
+   null,
+   [],
+   {},
    {
-      label: 'LUX Design',
+      label: null
+   },
+   {
+      label: ''
+   },
+   {
+      label: 1452
+   },
+   {
+      label: 'Programming'
+   },
+   {
+      label: 'Programming',
+      value: null
+   },
+   {
+      label: 'Programming',
+      value: 'random'
+   },
+   {
+      label: 'Programming',
+      value: Infinity
+   },
+   {
+      label: 'Programming',
+      value: NaN
+   },
+   {
+      label: 'Programming',
+      value: -120
+   },
+   {
+      label: 'Programming',
+      value: 120
+   },
+   {
+      label: 'Programming',
+      value: 50.148525145
+   },
+   {
+      label: ' ',
+      value: 30
+   },
+   {
+      label: 'Programming',
+      value: 100
+   },
+   {
+      label: 'Programming',
+      value: 0
+   },
+   {
+      label: 'UX design',
       value: 90
    },
    {
@@ -10,7 +69,7 @@ const progressBarData = [
    {
       label: 'Web development',
       value: 50
-   },
+   }, 
 ];
 
 export { progressBarData }
